df8e196e5d98ce62c1372d3b56055324b14526c6
krishjs <gplakrish@gmail.com>
2016-04-19 19:04:21 +0530

    cricle with range

--- src/Google.Maps.Init/wwwroot/js/map.js
+++ src/Google.Maps.Init/wwwroot/js/map.js
@@ -2,8 +2,10 @@
 (function (w) {
     w['kjsmap'] = {
         map: null,
+        Circle:undefined,
         mapOptions: {
-            minZoomLevel: 3
+            minZoomLevel: 3,
+            center: { lat: 13.046034415549146, lng: 80.321044921875 }
         },
         SetPosition: function (s, v) {
             var self = this;
@@ -58,8 +60,59 @@
                 }
             },
             Search: function (s, e) {
-
+                var start = this.FromPort.Marker.position;
+                var end = this.ToPort.Marker.position;
+                kjsmap.Direction.Service(!0,{
+                    origin: start,
+                    destination: end,
+                    travelMode: google.maps.DirectionsTravelMode.TRANSIT,
+                    transitOptions: { modes: [google.maps.TransitMode.TRAIN] }
+                });
             },
+            RangeChanged: function (s, e) {
+                kjsmap.Circle.setRadius(s.value * 1000);
+            }
+        },
+        RouteData:null,
+        RoutesView: function (f) {
+            if (f) {
+                if (this.RouteData === null) {
+                    var self = this;
+                    ajax.call({
+                        url: 'http://localhost:8081',
+                        isasync: true,
+                        type: 'GET',
+                        success: function (data) {
+                            self.RouteData = JSON.parse(data);
+                            kjsmap.map.data.addGeoJson(self.RouteData);
+                        }
+                    });
+                }
+                else {
+                    kjsmap.map.data.addGeoJson(this.RouteData);
+                }
+            }
+            else {
+                kjsmap.map.data.forEach(function (feature) { kjsmap.map.data.remove(feature); });
+            }
+        },
+        Direction: {
+            RenderMan: undefined,
+            ServiceGuy: undefined,
+            Service: function (d,options,callback) {
+                if (!this.ServiceGuy) { this.ServiceGuy = new google.maps.DirectionsService(); }
+                var self = this;
+                this.ServiceGuy.route(options, function (r) {
+                    if (d) { self.Display(!0, r); }
+                    else { callback.call(kjsmap, r); }
+                });
+            },
+            Display: function (n,r) {
+                var renderGuy = (!n && this.RenderMan) ? this.RenderMan : new google.maps.DirectionsRenderer();
+                renderGuy.setMap(kjsmap.map);
+                renderGuy.setDirections(r);
+                if (!n && !this.RenderMan) { this.RenderMan = renderGuy; }
+            }
         }
     };
     w['ajax'] = function () {
@@ -98,34 +151,28 @@ function initMap() {
         var posEnd = new google.maps.LatLng(85, 180);
         var allowedBounds = new google.maps.LatLngBounds(posStart, posEnd);
 
+        kjsmap.Circle = new google.maps.Circle({
+            strokeColor:'#000000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#F0FFFF',
+            fillOpacity: 0.2,
+            center: { lat: 13.089844954062787, lng: 80.29838562011719 },
+            radius: 500,
+            map: kjsmap.map
+        });
         google.maps.event.addListener(kjsmap.map, "click", function (e) {
             console.log("Lat: " + e.latLng.lat());
             console.log("Lng: " + e.latLng.lng());
         });
-
-        google.maps.event.addListener(kjsmap.map, 'center_changed', function () { checkBounds(); });
-
-        function checkBounds() {
+        google.maps.event.addListener(kjsmap.map, 'center_changed', function (e) {
             if ((allowedBounds.getNorthEast().lat() > (kjsmap.map.getBounds().getNorthEast().lat()))
-                && (allowedBounds.getSouthWest().lat() < (kjsmap.map.getBounds().getSouthWest().lat()))) {
+                    && (allowedBounds.getSouthWest().lat() < (kjsmap.map.getBounds().getSouthWest().lat()))) {
                 lastValidCenter = kjsmap.map.getCenter();
                 return;
             }
             kjsmap.map.panTo(lastValidCenter);
-        }
-    }
-    document.getElementById('#search');
+        });
+    }    
 };
-function detectBrowser() {
-    debugger;
-    var useragent = navigator.userAgent;
-    var mapdiv = document.getElementById("map");
-    if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1) {
-        mapdiv.style.width = '100%';
-        mapdiv.style.height = '100%';
-    } else {
-        mapdiv.style.width = '600px';
-        mapdiv.style.height = '800px';
-    }
-}
-document.addEventListener("load", detectBrowser);
+
